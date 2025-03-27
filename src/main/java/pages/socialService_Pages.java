5e25d68b05a589c06efec5fe867de40446b2cf4e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-03-27 12:23:49 +0530

    Add files via upload

---
+++ src/main/java/pages/socialService_Pages.java
@@ -0,0 +1,167 @@
+package pages;
+
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class socialService_Pages extends StartupPage {
+	
+//	TC1-Locators
+	By usernameTextbox = null;
+	By passwordTextbox = null;
+	By signInButton = null;
+	By registeredPatientTextElement = null;
+//	TC2-Locators
+	By socialServiceModule = null;
+//	TC3-Locators
+	By registerNewSSUPatientButton = null;
+	By newSSUPatientRegistrationFormNameElement = null;
+//	TC4-Locators
+	By registrationButtonOfNewSSUPatientRegistrationForms = null;
+	By errorMeesageInLastNameTextFieldElement = null;
+//	TC5-Locators
+	By firstNameTextField = null;
+	By middelNameTextField = null;
+	By lastNameTextField = null;
+//	TC6-Locators
+	By registerButtonOfNewSSUPatientRegistrationForms = null;
+	By closeButtonOfNewSsuPatientRegistrationByElement = null;
+//	TC7-Locators
+	By SSUInformationWarningMessage = null;
+//	TC8-Locators
+	By hasTG_certificateDropdown = null;
+	By selectYesFromhasTG_certificateDropdown = null;
+	By targetGroupCertificateTypeTextbox = null;
+	By certificateNoTextbox = null;
+
+	String pageName = this.getClass().getSimpleName();
+	public socialService_Pages(WebDriver driver) {
+		super(driver);
+	}
+
+	/**@Test1.1
+	 * about this method loginTohealthAppByGivenValidCredetial() 
+	 * @param : Map<String, String>,get the data from Excel file (login) as required
+	 * @description : fill usernameTextbox & passwordTextbox and click on sign in button
+	 * @return : Boolean
+	 * @author : YAKSHA
+	 */
+	public Boolean loginToHealthAppByGivenValidCredetial(Map<String, String> expectedData) throws Exception {
+		
+		return false;
+	}
+
+	/**@Test1.2
+	 * about this method verifyTitleOfThePage() 
+	 * @param : null
+	 * @description : it will navigate to the URL and return the title of the current page.
+	 * @return : String
+	 * @author : YAKSHA
+	 */
+	public String verifyTitleOfThePage() throws Exception {
+
+		return null;
+	}
+
+	/**@Test1.3
+	 * about this method verifyURLOfThePage() 
+	 * @param : null
+	 * @description : it will return the URL of the current page.
+	 * @return : String
+	 * @author : YAKSHA
+	 */
+	public String verifyURLOfThePage() throws Exception {
+			
+		return null;
+	}
+
+	/**@Test2
+	 * about this method verifySocialServiceModuleisPresentAndGoToSocialServiceTab() 
+	 * @param : null
+	 * @description : Verify that SocialService module is present and Go to SocialService Tab.
+	 * @return : Boolean
+	 * @author : YAKSHA
+	 */
+	public Boolean verifySocialServiceModuleisPresentAndGoToSocialServiceTab() throws Exception {
+		
+		return false;
+	}
+
+	/**@Test3
+	 * about this method verifyRegisterNewSSUPatientButtonisPresentAndValidateFormName()
+	 * @param : null
+	 * @description : It will return the title of the form pop up by clicking the "Register New SSU Patient" button.
+	 * @return : String
+	 * @author : YAKSHA
+	 */
+	public String verifyRegisterNewSSUPatientButtonisPresentAndValidateFormName() throws Exception {
+		return null;
+	}
+
+	/**@Test4
+	 * about this method validateErrorMessageInLastNameTextfield() 
+	 * @param : null
+	 * @description : return the error message in Last Name text field without filling any data and clicking on "Register" Button
+	 * @return : String 
+  	 * @author : YAKSHA
+	 */
+	public String validateErrorMessageInLastNameTextfield() throws Exception {
+		
+		return null;
+	}
+
+	/**@Test5
+	 * about this method fillDataInTextfieldsAndVerifyEnteredDataInFirstName() 
+	 * @param : Map<String, String>,get the data from Excel file (expected_Data) as required
+	 * @description : Fill all the mandatory text fields which are present inside the New SSU Patient Registration popup and return the data which we have entered in First Name Field. 
+	 * @return : String
+	 * @author : YAKSHA
+	 */
+	public String fillDataInTextfieldsAndVerifyEnteredDataInFirstName(Map<String, String> expectedData) throws Exception {
+		
+		return null;
+	}
+
+
+	/**@Test6
+	 * about this method scrollToButtomOfThePageAndVerifyRegisterAndCloseButtonIsPresent()
+	 * @param : null
+	 * @description : scroll to the bottom of the page and verify that "Register" and "Close" button are present or not.
+	 * @return : Boolean
+	 * @author : YAKSHA
+	 */
+	public Boolean scrollToButtomOfThePageAndVerifyRegisterAndCloseButtonIsPresent() throws Exception {
+		
+		return false;
+	}
+
+	/**@Test7
+	 * about this method validateWarnningMessageOfSSUInformationSection() 
+	 * @param : Map<String, String>,get the data from Excel file (expected_Data) as required
+	 * @description : return the warning message displayed in "SSU Information" section.
+	 * @return : String
+	 * @author : YAKSHA
+	 */
+	public String validateWarnningMessageOfSSUInformationSection(Map<String, String> expectedData) throws Exception {
+		
+		return null;
+	}
+	
+	/**@Test8
+	 * about this method verifyTextboxIsPresentBySelectingYesFromHasTargetGroupCertificateDropdown() 
+	 * @param : Map<String, String>,get the data from Excel file (expected_Data) as required  
+	 * @description : it will return the value of "Has Target Group Certificate?" field.
+	 * @return : String
+	 * @author : YAKSHA
+	 */
+	public String verifyTextboxIsPresentBySelectingYesFromHasTargetGroupCertificateDropdown(Map<String, String> expectedData) throws Exception {
+
+		return null;
+	}
+
+
+
+	
+}
